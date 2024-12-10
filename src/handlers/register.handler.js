bee14e972d47cbc4720a1a2d72d2b867be3c647d
Jaeyoung <jyko1101@gmail.com>
2024-12-10 19:46:13 +0900

    refactor: register handler

--- src/handlers/register.handler.js
+++ src/handlers/register.handler.js
@@ -7,6 +7,7 @@
 
 import { v4 as uuidv4 } from 'uuid';
 import { addUser } from '../models/user.model.js';
+import { handleConnection, handleDisconnect } from './helper.js';
 
 const registerHandler = (io) => {
     io.on('connection', (socket) => {
@@ -15,10 +16,15 @@ const registerHandler = (io) => {
         const userUUID = uuidv4();
         // 사용자 추가
         addUser({ uuid: userUUID, socketId: socket.id });
+        createStage(uuerUUID);
+        // 접속시 유저 정보 생성 이벤트 처리
+        handleConnection(socket, userUUID);
 
+
+        // 메세지를 data 란 이름으로 handlerEvent 함수로 전달합니다.
+        socket.on('event', (data) => handleEvent(io, socket, data));
         // 접속 해제 시 이벤트 처리
         socket.on('disconnect', () => handleDisconnect(socket, userUUID));
-
     });
 };
 
