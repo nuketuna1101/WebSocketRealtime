4492b567bb20d0b57c825d71ae959476f6bf2c71
Jaeyoung <jyko1101@gmail.com>
2024-12-10 12:51:28 +0900

    feat: init assets loading

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createServer } from 'http';
 import initSocket from './init/socket.js';
+import { loadGameAssets } from './init/assets.js';
 
 const app = express();
 const server = createServer(app);
@@ -9,14 +10,24 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 // 소켓 시작
 initSocket(server);
 
+// test api
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
+    res.send('<h1>Hello World</h1>');
 });
 
+// 
 server.listen(PORT, async () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
+
+    try {
+        // Asset loading
+        const assets = await loadGameAssets();
+        console.log(assets);
+        console.log('Assets loaded successfully');
+    } catch (error) {
+        console.error('Failed to load game assets:', error);
+    }
 });
